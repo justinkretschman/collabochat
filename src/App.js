35c6c86c27403ad75da35f180f32ec55c74a79de
justinkretschman <73443789+justinkretschman@users.noreply.github.com>
2020-12-29 13:46:42 -0500

    change symbols to collabo

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>⚛️🔥💬</h1>
+        <h1>Collabo</h1>
         <SignOut />
       </header>
 
@@ -51,7 +51,7 @@ function SignIn() {
   return (
     <>
       <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the community guidelines or you will be banned for life!</p>
+      <p>What do you think about Collabo?</p>
     </>
   )
 
@@ -103,7 +103,7 @@ function ChatRoom() {
 
       <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
 
-      <button type="submit" disabled={!formValue}>🕊️</button>
+      <button type="submit" disabled={!formValue}>©</button>
 
     </form>
   </>)
